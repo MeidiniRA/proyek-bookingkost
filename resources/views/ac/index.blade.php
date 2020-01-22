45bfb2ad5d64b9537c78138df36a66030756e318
Princessa Virginia Addyska <58239554+pvaddyska@users.noreply.github.com>
2020-01-22 15:08:48 +0700

    ac kipas

--- resources/views/ac/index.blade.php
+++ resources/views/ac/index.blade.php
@@ -29,7 +29,7 @@
   </head>
   <body>
  @include('layout.header')
-  <div class="site-blocks-cover overlay" style="background-image: url(images/hero_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+  <div class="site-blocks-cover overlay" style="background-image: url({{asset('front/images/hero_1.jpg')}});" data-aos="fade" data-stellar-background-ratio="0.5">
         <div class="container">
           <div class="row align-items-center justify-content-center">
             <div class="col-md-7 text-center" data-aos="fade">
